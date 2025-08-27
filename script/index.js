a7dd7a584328ce0faec3a746f1aa2905cb1e1d57
Habib0088 <sarkerbatch2k19@gmail.com>
2025-08-27 11:21:27 +0600

    history listing done

--- script/index.js
+++ script/index.js
@@ -1,5 +1,5 @@
 // Increase Heart START Here
-let hearts=document.getElementsByClassName('card');
+let hearts=document.getElementsByClassName('fa-heart');
 for(let heart of hearts){
     heart.addEventListener('click', function(){
         let totalHeatCount=document.getElementById('heart').innerText;
@@ -13,12 +13,51 @@ for(let heart of hearts){
 
 }
 // Increase Heart End Here
-// Call Coin Area
+// Call Coin Start Area
 
 let calls=document.getElementsByClassName('call');
 for(let call of calls){
     call.addEventListener('click', function(){
-        console.log('call clicked');
+        
+        let defaultCoin=document.getElementById('coin').innerText;
+        let defaultCoinNumber=Number(defaultCoin);
+        if(defaultCoinNumber >= 20){
+            alert('you are calling');
+                let newCoin=defaultCoinNumber - 20;
+        document.getElementById('coin').innerText=newCoin;
+            // Target card info for add into history list
+            let name=call.parentNode.parentNode.children[1].innerText;
+            let serviceNumber=call.parentNode.parentNode.children[3].innerText;
+            let time=new Date().toLocaleTimeString();
+            
+            
+            
+            // Target card info for add into history list
+        let historyArea=document.getElementById('history-list-area');
+        let newAddedhistory=document.createElement('div');
+        newAddedhistory.innerHTML=`
+            <div class="area flex justify-between items-center bg-[#FAFAFA] my-4 p-2 rounded-xl" id="history-list-area">
+                    <div class="area-left">
+                        <p class="font-bold text-sm" >${name}</p>
+                        <p class="text-[#5C5C5C]">${serviceNumber}</p>
+                    </div>
+                    <div class="area-right">
+                        <p class="text-sm">${time}</p>
+                    </div>
+                 </div>
+        `
+        historyArea.appendChild(newAddedhistory);
+            
+        }
+
+
+
+
+        else{
+                alert('You Have No Enough Coin For Making A Call');
+                return
+            }
+    
     })
     
 }
